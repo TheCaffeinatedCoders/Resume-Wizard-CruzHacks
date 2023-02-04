c575dfaf1ebf328dda03fd9dc3bc7632bce5533b
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-04 15:48:59 -0800

    Added info into routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,12 @@
 import Home from './routes/Home.svelte';
 import NotFound from './routes/NotFound.svelte';
-import Test from './routes/Test.svelte'
+import Test from './routes/Test.svelte';
+import Info from './routes/Info.svelte';
 
 export default {
     '/': Home,
     '/test': Test,
+    '/info': Info,
     // The catch-all route must always be last
     '*': NotFound
 };
