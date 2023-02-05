8c6e22744f6014d013a9ee692662e2a6e10437ae
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-04 18:05:39 -0800

    Completion example

---
+++ frontend/src/routes/Completion.svelte
@@ -0,0 +1,23 @@
+<script>
+    let response = "";
+
+    async function getResponse() {
+        try {
+            const data = await fetch('http://127.0.0.1:8000');
+            response = await data.json();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+</script>
+
+<h1>Here is a sample completion response</h1>
+<label for="prompt">Prompt:</label>
+<input
+    type="text"
+    id="prompt"
+    name="prompt"
+    placeholder="Insert some text here"
+/>
+<button type="button" on:click={getResponse}>Click Me!</button>
+<p>Response: {response}</p>
