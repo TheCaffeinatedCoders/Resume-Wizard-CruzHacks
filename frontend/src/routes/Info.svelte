68f3edbff00b45c74baabf737a7909c073e7e6e6
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-05 00:53:26 -0800

    Window cool

--- frontend/src/routes/Info.svelte
+++ frontend/src/routes/Info.svelte
@@ -1,44 +1,108 @@
 <script>
-    let count = 0;
-    let dictionary = {
-    name: "",
-    age: "",
-    phone: "",
-  };
-//   Create a funciton to append a new key and empty value into the dictionary
-    function addKey() {
-        dictionary[count] = "";
-        count++;
-        dictionary = {...dictionary};
-    }
-    function updateValue(key, event) {
-    dictionary[key] = event.target.value;
-  }
+    // let count = 0;
+    // let dictionary = {
+    //     name: "",
+    //     age: "",
+    //     phone: "",
+    // };
+    let sections = ["personalInfo", "education", "workExperience", "projects", "skills"]
+    let currentSection = "personalInfo";
+    let userData = {
+        personalInfo: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            objective: "",
+        },
+        education: [
+            {
+                school: "",
+                degree: "",
+                graduation: "",
+            },
+        ],
+        workExperience: [
+            {
+                company: "",
+                position: "",
+                start: "",
+                end: "",
+            },
+        ],
+        projects: [
+            {
+                name: "",
+                description: "",
+                stack: "",
+                start: "",
+                end: "",
+            },
+        ],
+        skills: [""],
+    };
+    //   Create a funciton to append a new key and empty value into the dictionary
+    // function addKey() {
+    //     dictionary[count] = "";
+    //     count++;
+    //     dictionary = { ...dictionary };
+    // }
+    // function updateValue(key, event) {
+    //     dictionary[key] = event.target.value;
+    // }
 
-  // Function to print all the data at the bottom of the screen on button press
-    function printData() {
-        let data = "";
-        for (let key in dictionary) {
-        data += key + ": " + dictionary[key] + "\n";
-        }
-        alert(data);
-    }
+    // Function to print all the data at the bottom of the screen on button press
+    // function printData() {
+    //     let data = "";
+    //     for (let key in dictionary) {
+    //         data += key + ": " + dictionary[key] + "\n";
+    //     }
+    //     alert(data);
+    // }
 </script>
 
 <h1>Resume Wizard</h1>
-<h2>We are going to need some basic info from you to begin formatting your resume</h2>
+<h2>
+    We are going to need some basic info from you to begin formatting your
+    resume
+</h2>
 <form>
-{#each Object.entries(dictionary) as [key, value]}
-  <div>
-    <label for={key}>{key}:</label>
-    <input id={key} bind:value={value} on:input={(event) => updateValue(key, event)} />
-  </div>
-{/each}
+    {#each Object.entries(userData) as [key, value]}
+        {#if key == currentSection}
+                <h3>{currentSection}</h3>
+                <!-- <p>{value.jsonStringify()}</p> -->
+                <p>{JSON.stringify(value)}</p>
+                <button on:click={() => (currentSection = sections[sections.indexOf(currentSection) + 1])}>Next</button>
+        <!-- {:else if key === 'education'}
+            <h3>Education</h3>
+            <p>{value}</p>
+        {:else if key === 'workExperience'}
+            <h3>Work Experience</h3>
+            <p>{value}</p>
+        {:else if key === 'projects'}
+            <h3>Projects</h3>
+            <p>{value}</p>
+        {:else if key === 'skills'}
+            <h3>Skills</h3>
+            <p>{value}</p> -->
+        {/if}
+    {/each}
 </form>
+<!-- <form>
+    {#each Object.entries(userData) as [key, value]}
+        <div>
+            <label for={key}>{key}:</label>
+            <input
+                id={key}
+                bind:value
+                on:input={(event) => updateValue(key, event)}
+            />
+        </div>
+    {/each}
+</form> -->
 
-<button on:click={addKey}>Add Key</button>
-<button on:click={printData}>Print Data</button>
-
+<!-- <button on:click={addKey}>Add Key</button>
+<button on:click={printData}>Print Data</button> -->
 
 <!-- <form>
     <h3>Personal Information</h3>
