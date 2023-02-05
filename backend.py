b8306fdd29dd6e8914bc1ad51499106568a25c73
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-04 22:44:08 -0800

    generate doc JSON communication

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import openai
 import json
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from jinja2 import FileSystemLoader
 from latex.jinja2 import make_env
 from pdflatex import PDFLaTeX
@@ -12,16 +13,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 prompt = "I am a highly intelligent resume builder bot. When provided with a basic description from the user about a particular section on a resume, I generate a more beautifully written response that would look good on a resume. I make sure that in my responses I am not inventing any information or using any information that was not explicitly provided by the user. My responses are concise, accurate, and clearly represent the same meaning as the user's input, but written in a more beautiful, reputable way. My responses use exclusively user provided information, without inventing or using any information that was not explicitly provided by the user."
 
@@ -43,20 +34,24 @@ async def root():
 async def get_ai_response(field_name : str, data : str):
     field_name = unquote(field_name)
     data = unquote(data)
+    print(field_name, data)
     return openai.Completion.create(
         model = 'text-davinci-003',
         prompt = f'{prompt}\n{field_name}: {data}',
         max_tokens = 150,
-        temperature = 1
+        temperature = .4
     ).choices[0].text
 
-@app.get("/generate-doc")
-async def generate_doc(data: str):
-    data = json.loads(data)
-
+@app.post("/generate-doc")
+async def generate_doc(data: dict):
     env = make_env(loader=FileSystemLoader('.'))
     template = env.get_template('template.tex')
 
     rendered_doc = template.render(**data)
+    response = JSONResponse(content=rendered_doc)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
-    return PDFLaTeX.from_string(rendered_doc).getpdf()
+
+
+    #return PDFLaTeX.from_string(rendered_doc).getpdf()
--- frontend/src/routes/Completion.svelte
+++ frontend/src/routes/Completion.svelte
@@ -1,11 +1,39 @@
 <script>
     let response = "";
-
+    const endpoint = "http://127.0.0.1:8000/get-ai-response";
+    let userInput = "";
     async function getResponse() {
         try {
-            const data = await fetch('http://127.0.0.1:8000/');
+            const data = await fetch(
+                endpoint + "?field_name=&data=" + encodeURIComponent(userInput)
+            );
+            // const data = await fetch ('http://127.0.0.1:8000/get-ai-response?field_name=education&data=I%20am%20a%20student%20at%20the%20University%20of%20Washington%20studying%20computer%20science%20and%20mathematics.');
             response = await data.json();
             console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function getDoc() {
+        let UserArgs = {
+            name: "John Doe",
+            phone: "555-555-1212",
+            email: "test@example.com",
+            github: "github.com/test",
+            linkedin: "linkedin.com/test",
+        };
+
+        try {
+            let response = await fetch(`http://127.0.0.1:8000/generate-doc`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(UserArgs),
+            });
+            let jsonData = await response.json();
+            console.log(jsonData);
         } catch (error) {
             console.error(error);
         }
@@ -15,10 +43,13 @@
 <h1>Here is a sample completion response</h1>
 <label for="prompt">Prompt:</label>
 <input
+    bind:value={userInput}
     type="text"
     id="prompt"
     name="prompt"
     placeholder="Insert some text here"
 />
 <button type="button" on:click={getResponse}>Click Me!</button>
-<p>Response: {response.message}</p>
+<p>Response: {response}</p>
+
+<button on:click={getDoc}>Generate Doc</button>
