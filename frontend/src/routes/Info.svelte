3752e4d91c13514796ce9b2771d86530a41a2afa
CarsonWebster <CarsonWebster@hotmail.com>
2023-02-05 01:48:40 -0800

    We did it

--- frontend/src/routes/Info.svelte
+++ frontend/src/routes/Info.svelte
@@ -1,10 +1,4 @@
 <script>
-    // let count = 0;
-    // let dictionary = {
-    //     name: "",
-    //     age: "",
-    //     phone: "",
-    // };
     let sections = [
         "personalInfo",
         "education",
@@ -14,13 +8,14 @@
     ];
     let currentSection = "personalInfo";
     let userData = {
-        personalInfo: {
-            firstName: "Carson",
-            lastName: "",
-            email: "",
-            phone: "",
-            objective: "",
-        },
+        personalInfo: [
+            {
+                name: "",
+                email: "",
+                phone: "",
+                objective: "",
+            },
+        ],
         education: [
             {
                 school: "",
@@ -47,23 +42,20 @@
         ],
         skills: [""],
     };
-    //   Create a funciton to append a new key and empty value into the dictionary
-    // function addKey() {
-    //     dictionary[count] = "";
-    //     count++;
-    //     dictionary = { ...dictionary };
-    // }
-    // function updateValue(key, event) {
-    //     dictionary[key] = event.target.value;
-    // }
 
-    // Function to print all the data at the bottom of the screen on button press
-    // function printData() {
-    //     let data = "";
-    //     for (let key in dictionary) {
-    //         data += key + ": " + dictionary[key] + "\n";
-    //     }
-    //     alert(data);
+    $: currentSectionData = userData[currentSection];
+
+    // function nextSection() {
+    //     if (currentSection === "personalInfo") {
+    //             return;
+    //         }
+    //         currentSection = sections[sections.indexOf(currentSection) - 1]; 
+    // }
+    // function previousSection() {
+    //     if (currentSection === "skills") {
+    //             alert(userData);
+    //         }
+    //         currentSection = sections[sections.indexOf(currentSection) + 1]; 
     // }
 </script>
 
@@ -72,37 +64,103 @@
     We are going to need some basic info from you to begin formatting your
     resume
 </h2>
-<form>
-    {#each Object.entries(userData) as [key, value]}
-        {#if key == currentSection}
-            <h3>{currentSection}</h3>
-            <!-- <p>{value.jsonStringify()}</p> -->
-            <p>{JSON.stringify(value)}</p>
-            {#each Object.entries(value) as [prompt, input]}
-                <div>
-                    <label for={prompt}>{prompt}:</label>
-                    <input
-                        id={prompt}
-                        bind:value={userData.currentSection.input}
-                    />
-                </div>
-            {/each}
-        {/if}
-        <button
-            on:click={() =>
-                (currentSection =
-                    sections[sections.indexOf(currentSection) + 1])}>Next</button>
-    {/each}
-</form>
-<!-- <form>
-    {#each Object.entries(userData) as [key, value]}
-        <div>
-            <label for={key}>{key}:</label>
-            <input
-                id={key}
-                bind:value
-                on:input={(event) => updateValue(key, event)}
-            />
-        </div>
-    {/each}
-</form> -->
+
+<h3>{currentSection}</h3>
+{#if currentSection === "personalInfo"}
+    <div>
+        <label for="name">Name:</label>
+        <input id="name" bind:value={currentSectionData.name} />
+    </div>
+    <div>
+        <label for="email">Email:</label>
+        <input id="email" bind:value={currentSectionData.email} />
+    </div>
+    <div>
+        <label for="phone">Phone:</label>
+        <input id="phone" bind:value={currentSectionData.phone} />
+    </div>
+    <div>
+        <label for="objective">Objective:</label>
+        <input id="objective" bind:value={currentSectionData.objective} />
+    </div>
+{:else if currentSection === "education"}
+    <div>
+        <label for="school">School:</label>
+        <input id="school" bind:value={currentSectionData.school} />
+    </div>
+    <div>
+        <label for="degree">Degree:</label>
+        <input id="degree" bind:value={currentSectionData.degree} />
+    </div>
+    <div>
+        <label for="graduation">Graduation:</label>
+        <input id="graduation" bind:value={currentSectionData.graduation} />
+    </div>
+{:else if currentSection === "workExperience"}
+    <div>
+        <label for="company">Company:</label>
+        <input id="company" bind:value={currentSectionData.company} />
+    </div>
+    <div>
+        <label for="position">Position:</label>
+        <input id="position" bind:value={currentSectionData.position} />
+    </div>
+    <div>
+        <label for="start">Start:</label>
+        <input id="start" bind:value={currentSectionData.start} />
+    </div>
+    <div>
+        <label for="end">End:</label>
+        <input id="end" bind:value={currentSectionData.end} />
+    </div>
+{:else if currentSection === "projects"}
+    <div>
+        <label for="name">Name:</label>
+        <input id="name" bind:value={currentSectionData.name} />
+    </div>
+    <div>
+        <label for="description">Description:</label>
+        <input id="description" bind:value={currentSectionData.description} />
+    </div>
+    <div>
+        <label for="stack">Stack:</label>
+        <input id="stack" bind:value={currentSectionData.stack} />
+    </div>
+    <div>
+        <label for="start">Start:</label>
+        <input id="start" bind:value={currentSectionData.start} />
+    </div>
+    <div>
+        <label for="end">End:</label>
+        <input id="end" bind:value={currentSectionData.end} />
+    </div>
+{:else if currentSection === "skills"}
+    <div>
+        <label for="skill">Skill:</label>
+        <input id="skill" bind:value={currentSectionData[0]} />
+    </div>
+{/if}
+<div>
+    <button
+        type="button"
+        on:click={() => {
+            if (currentSection === "personalInfo") {
+                return;
+            }
+            currentSection = sections[sections.indexOf(currentSection) - 1];
+        }}
+    >
+        Previous
+    </button>
+    <button
+        type="button"
+        on:click={() => {
+            if (currentSection === "skills") {
+                alert(JSON.stringify(userData));
+            }
+            currentSection = sections[sections.indexOf(currentSection) + 1];
+        }}
+    >
+        Next
+    </button>
+</div>
