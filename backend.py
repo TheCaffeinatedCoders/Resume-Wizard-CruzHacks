ed7dfe474b0c7de79dc14ecb55f0b19d4fa79b7e
cowen27 <calvin.owen27@gmail.com>
2023-02-04 18:28:22 -0800

    Updated backend for generate_doc

--- backend.py
+++ backend.py
@@ -5,18 +5,33 @@ from fastapi import FastAPI
 from jinja2 import FileSystemLoader
 from latex.jinja2 import make_env
 from pdflatex import PDFLaTeX
+from fastapi.middleware.cors import CORSMiddleware
+from urllib.parse import unquote
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 prompt = "I am a highly intelligent resume builder bot. When provided with a basic description from the user about a particular section on a resume, I generate a more beautifully written response that would look good on a resume. I make sure that in my responses I am not inventing any information or using any information that was not explicitly provided by the user. My responses are concise, accurate, and clearly represent the same meaning as the user's input, but written in a more beautiful, reputable way. My responses use exclusively user provided information, without inventing or using any information that was not explicitly provided by the user."
 
 @app.get("/")
 async def root():
     return { "message": "Hello root" }
 
-@app.get("/get-ai-response/{field_name}/{data}")
+@app.get("/get-ai-response")
 async def get_ai_response(field_name : str, data : str):
+    field_name = unquote(field_name)
+    data = unquote(data)
     return openai.Completion.create(
         model = 'text-davinci-003',
         prompt = f'{prompt}\n{field_name}: {data}',
